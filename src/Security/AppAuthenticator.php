c569a1787694cdd44fb7cb6c5fdd2b3f9c0fed45
Tresor-ilunga <19im065@esisalama.org>
2023-11-27 00:34:35 +0100

    fix(security): update URL

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -67,7 +67,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_profile'));
+        return new RedirectResponse($this->urlGenerator->generate('app_profile_index'));
     }
 
     /**
